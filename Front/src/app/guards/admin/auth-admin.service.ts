bb688b0d6cd175482be35a5f61f7814c2cb9d2a5
abdelkrimmajdi <majdi.abdelkrim@esprit.tn>
2024-04-01 00:56:10 +0100

    majdiii

--- Front/src/app/guards/admin/auth-admin.service.ts
+++ Front/src/app/guards/admin/auth-admin.service.ts
@@ -15,7 +15,7 @@ export class AuthAdminService {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.userconnect.role==Role.ADMIN)
+    if (this.userconnect.role==Role.ADMIN||this.userconnect.role==Role.NUTRITIONISTE||this.userconnect.role==Role.COACH||this.userconnect.role==Role.MENTOR)
       return true
     else {
       this.route.navigateByUrl('/**')
